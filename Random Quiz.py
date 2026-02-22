57c0e18f0ae2478160606b5f9fc60828bf565f50
Chandana Jagadish <yaschandana@gmail.com>
2026-02-22 23:19:32 +0530

    Add files via upload
    Generate Random Quiz Files
    
    Say you’re a geography teacher with 35 students in your class and you want to give a pop quiz on Indian state capitals. Alas, your class has a few bad eggs in it, and you can’t trust the students not to cheat. You’d like to randomize the order of questions so that each quiz is unique, making it impossible for anyone to crib answers from anyone else. Of course, doing this by hand would be a lengthy and boring affair. Fortunately, you know some Python.
    
    Here is what the program does:
    
    Creates 35 different quizzes
    Creates 28 multiple-choice questions for each quiz, in random order
    Provides the correct answer and three random wrong answers for each question, in random order
    Writes the quizzes to 35 text files
    Writes the answer keys to 35 text files
    This means the code will need to do the following:
    
    Store the states and their capitals in a dictionary.
    Call open(), write(), and close() for the quiz and answer key text files.
    Use random.shuffle() to randomize the order of the questions and multiple-choice options.

---
+++ Random Quiz.py
@@ -0,0 +1,62 @@
+import random
+capitals={
+    "Andhra Pradesh": "Amaravati",
+    "Arunachal Pradesh": "Itanagar",
+    "Assam": "Dispur",
+    "Bihar": "Patna",
+    "Chhattisgarh": "Raipur",
+    "Goa": "Panaji",
+    "Gujarat": "Gandhinagar",
+    "Haryana": "Chandigarh",
+    "Himachal Pradesh": "Shimla",
+    "Jharkhand": "Ranchi",
+    "Karnataka": "Bengaluru",
+    "Kerala": "Thiruvananthapuram",
+    "Madhya Pradesh": "Bhopal",
+    "Maharashtra": "Mumbai",
+    "Manipur": "Imphal",
+    "Meghalaya": "Shillong",
+    "Mizoram": "Aizawl",
+    "Nagaland": "Kohima",
+    "Odisha": "Bhubaneswar",
+    "Punjab": "Chandigarh",
+    "Rajasthan": "Jaipur",
+    "Sikkim": "Gangtok",
+    "Tamil Nadu": "Chennai",
+    "Telangana": "Hyderabad",
+    "Tripura": "Agartala",
+    "Uttar Pradesh": "Lucknow",
+    "Uttarakhand": "Dehradun",
+    "West Bengal": "Kolkata"
+}
+
+#Create 35 different files
+for i in range(35):
+   quiz_file=open(f'capitalsquiz{i+1}.txt',"w",encoding='UTF-8')
+   ans_file=open(f'capitalsquiz_ans{i+1}','w',encoding='UTF-8')
+   quiz_file.write('Name:\n\n,date:\n\n,Period:\n\n')
+   quiz_file.write((' '*20)+f'Quizzzz(Form{i+1})')
+   quiz_file.write('\n\n')
+   
+   states=list(capitals.keys())
+   random.shuffle(states)
+                   
+#Create 50 random questions
+   for j in range(28):
+       correct_ans=capitals[states[j]]
+       wrong_ans=list(capitals.values())
+       del wrong_ans[wrong_ans.index(correct_ans)]
+       wrong_ans=random.sample(wrong_ans,3)
+       answers=wrong_ans+[correct_ans]
+       random.shuffle(answers)
+    
+#Creating each question with choices
+       quiz_file.write(f'{j+1}.Capital of {states[j]}:\n')
+       for k in range(4):
+           quiz_file.write(f"   {'ABCD'[k]}.{answers[k]}\n")
+       quiz_file.write('\n')
+       ans_file.write(f"{i+1}.{'ABCD'[answers.index(correct_ans)]}")
+ans_file.close()
+quiz_file.close()
+    
+     
